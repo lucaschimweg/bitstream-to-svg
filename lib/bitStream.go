9bf5cb92658537b1edc29b3772227b5b2d459ea7
lucaschimweg <l.schimweg@gmail.com>
2019-10-14 19:20:24 +0200

    restructure bitStream

--- lib/bitStream.go
+++ lib/bitStream.go
@@ -5,3 +5,72 @@ type BitStream []bool
 // Double the length, always one before, one part from beginning to middle of phase, one from middle to end
 // -1, 0 or 1
 type VisibleBitStream []int8
+
+type BitStreamConverter func(stream BitStream)VisibleBitStream
+
+func NrzConvert(stream BitStream) VisibleBitStream {
+	res := make(VisibleBitStream, len(stream)*2)
+
+	for i := 0; i < len(stream); i++ {
+		if stream[i] {
+			res[2*i] = 1
+			res[2*i+1] = 1
+		} else {
+			res[2*i] = -1
+			res[2*i+1] = -1
+		}
+	}
+	return res
+}
+
+func ManchesterConvert(stream BitStream) VisibleBitStream {
+	res := make(VisibleBitStream, len(stream)*2)
+
+	for i := 0; i < len(stream); i++ {
+		if stream[i] {
+			res[2*i] = 1
+			res[2*i+1] = -1
+		} else {
+			res[2*i] = -1
+			res[2*i+1] = 1
+		}
+	}
+	return res
+}
+
+func NrziConvert(stream BitStream) VisibleBitStream {
+	res := make(VisibleBitStream, len(stream)*2)
+
+	var symb int8 = -1
+
+	for i := 0; i < len(stream); i++ {
+		if stream[i] {
+			res[2*i] = symb
+			symb = -symb
+			res[2*i+1] = symb
+		} else {
+			res[2*i] = symb
+			res[2*i+1] = symb
+		}
+	}
+	return res
+}
+
+func AmiConvert(stream BitStream) VisibleBitStream {
+	res := make(VisibleBitStream, len(stream)*2)
+
+	var symb int8 = -1
+
+	for i := 0; i < len(stream); i++ {
+		if stream[i] {
+			res[2*i] = symb
+			res[2*i+1] = symb
+			symb = -symb
+		} else {
+			res[2*i] = 0
+			res[2*i+1] = 0
+		}
+	}
+	return res
+}
+
